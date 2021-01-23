c7ba53399577b2cf706f402c8116e554d902c3fe
James Orson <jamesaorson@gmail.com>
2021-01-22 18:25:41 -0800

    Read input asynchronously

--- src/koms.nim
+++ src/koms.nim
@@ -1,15 +1,27 @@
 import os
 import parseopt
 import strformat
+import threadpool
+
 
 type
   Args = tuple
     address: string
+    is_valid: bool
 
-proc connect(address: string) =
+proc connect(address: string): bool =
   echo fmt"Connecting to {address}"
+  true
 
-proc parseArgs: Args =
+proc send(message: string): bool =
+  echo fmt"Sending: {message}"
+  true
+
+proc handle_message: bool =
+  let message = spawn stdin.read_line()
+  result = (^message).send()
+
+proc parse_args: Args =
   if paramCount() == 0:
     quit("Please specify the server address (e.g. ./client localhost)")
   
@@ -21,14 +33,22 @@ proc parseArgs: Args =
       of "":
         echo "Option: ", parser.key
       else:
-        result = (parser.val.string,)
+        result = (parser.val.string, true)
     of cmdEnd:
       break
     else:
       continue
 
 when isMainModule:
-  echo "Koms has started..."
   let args = parseArgs()
   
-  args.address.connect()
+  if not args.is_valid:
+    quit()
+  
+  echo "Koms has started..."
+  if not args.address.connect():
+    quit("Failed to connect!")
+  echo fmt"Connected to {args.address}"
+
+  while handle_message():
+    continue
